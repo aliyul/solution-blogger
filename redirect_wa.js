52663558a291426f647da8b345665d9213005c56
aliyul <waliprogrammer@gmail.com>
2026-08-18 15:31:43 +0700

    Update redirect_wa.js

--- redirect_wa.js
+++ redirect_wa.js
@@ -3,13 +3,13 @@
     const redirectMap = {
      //INI REDIRECT DARU NOMOR ASAL KE YANG BARU JIKA MAU TINGGAL SETTTING AJA SESUAI NOMOR ASAL DAN BARU NYA
       "https://wa.me/6283839000968": "https://wa.me/628218044304",
-      "href='https://wa.link/mz5dsa": "https://wa.me/628218044304"
+      "https://wa.link/mz5dsa": "https://wa.me/628218044304"
      // "https://wa.me/6283839002968": "https://wa.me/6281234560003"
     };
 
     // Ambil semua link <a>
-    const allLinks = document.querySelectorAll("a[href^='https://wa.me/']");
-
+    //const allLinks = document.querySelectorAll("a[href^='https://wa.me/']");
+    const allLinks = document.querySelectorAll("a[href*='wa.me/'], a[href*='wa.link/']");
     allLinks.forEach(function (link) {
       const originalHref = link.getAttribute("href");
 
